426561e9de824e9e108be0e3add8dd9708002914
David <ibdavid@gmx.net>
2021-09-19 15:14:17 -0500

    Added List All Vets on Vets Index Page, closes #22

--- pet-clinic-web/src/main/java/com/springframework/spring5petclinic/controllers/VetIndexController.java
+++ pet-clinic-web/src/main/java/com/springframework/spring5petclinic/controllers/VetIndexController.java
@@ -1,13 +1,23 @@
 package com.springframework.spring5petclinic.controllers;
 
+import com.springframework.spring5petclinic.model.Vet;
+import com.springframework.spring5petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetIndexController {
 
+    private final VetService<Vet, Long> vetService;
+
+    public VetIndexController(VetService<Vet, Long> vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets/", "vets/index", "vets", "vets/index.html"})
-    public String vetsIndex() {
+    public String vetsIndex(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
