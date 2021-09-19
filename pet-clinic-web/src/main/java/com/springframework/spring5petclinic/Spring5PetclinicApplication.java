20d79e30234957596f73b04100a51e431c41b13d
David <ibdavid@gmx.net>
2021-09-19 12:30:42 -0500

    Implemented SpringConfig for Services, closes #20.

--- pet-clinic-web/src/main/java/com/springframework/spring5petclinic/Spring5PetclinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/spring5petclinic/Spring5PetclinicApplication.java
@@ -2,8 +2,10 @@ package com.springframework.spring5petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.springframework.spring5petclinic"})
 public class Spring5PetclinicApplication {
 
 	public static void main(String[] args) {
