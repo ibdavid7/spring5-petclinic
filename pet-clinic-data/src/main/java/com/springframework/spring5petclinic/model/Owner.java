4fd112bcb3f5480c747b3005a44cecc5d344b60a
David <ibdavid@gmx.net>
2021-10-03 12:42:25 -0500

    Update Owner Entity with Pets set, closes #26

--- pet-clinic-data/src/main/java/com/springframework/spring5petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/spring5petclinic/model/Owner.java
@@ -1,4 +1,20 @@
 package com.springframework.spring5petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
+    }
 }
