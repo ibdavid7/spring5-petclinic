25b613c2734f7d5b75d2438993f9e948b4eef6f8
David <ibdavid@gmx.net>
2021-10-26 21:47:31 -0500

    Implement Base Entity (JPA) loses #35

--- pet-clinic-data/src/main/java/com/springframework/spring5petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/spring5petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.springframework.spring5petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private static final long serialVersionUID = 1L;
 
     public BaseEntity() {
