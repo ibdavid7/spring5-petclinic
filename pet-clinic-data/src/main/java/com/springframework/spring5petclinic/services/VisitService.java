b462ccf51a1633ae58067f98fd27cefa1d3f82b4
David <ibdavid@gmx.net>
2021-10-30 13:28:40 -0500

    Created Visit Map Service, closes #46

---
+++ pet-clinic-data/src/main/java/com/springframework/spring5petclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5petclinic.services;
+
+import com.springframework.spring5petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/spring5petclinic/services/map/VisitMapServiceImpl.java
@@ -0,0 +1,21 @@
+package com.springframework.spring5petclinic.services.map;
+
+import com.springframework.spring5petclinic.model.Visit;
+import com.springframework.spring5petclinic.services.VisitService;
+
+public class VisitMapServiceImpl extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getOwner().getId() == null || visit.getPet().getPetType() == null ||
+                visit.getPet().getPetType().getId() == null) {
+            throw new IllegalArgumentException("Using Transient Objects that are not persisted, persist transient " +
+                    "objects first");
+        }
+        return super.save(visit);
+    }
+
+}
